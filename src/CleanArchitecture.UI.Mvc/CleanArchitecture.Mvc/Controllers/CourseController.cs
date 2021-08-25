5b7e7c951b6931e81a6cbde16e7c1e8316de52b6
Kosto <kosto5o@gmail.com>
2021-08-25 16:33:21 +0200

    Views, Auth

---
+++ src/CleanArchitecture.UI.Mvc/CleanArchitecture.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CleanArchitecture.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CleanArchitecture.Mvc.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService ?? throw new ArgumentNullException(nameof(courseService));
+        }
+
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            var model = _courseService.GetCourses();
+
+            return View(model);
+        }
+    }
+}
