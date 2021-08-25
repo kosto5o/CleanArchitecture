f1e13e9a12278398b8633b829e56a635042f52f5
Kosto <kosto5o@gmail.com>
2021-08-25 14:15:36 +0200

    Application layer

---
+++ src/Application/CleanArchitecture.Application/Interfaces/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using CleanArchitecture.Application.ViewModels;
+using CleanArchitecture.Domain.Models;
+
+namespace CleanArchitecture.Application.Interfaces
+{
+    public interface ICourseService
+    {
+        IEnumerable<CourseViewModel> GetCourses();
+    }
+}
